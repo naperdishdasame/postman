398a452b5580f051fd3b263fd7866b3cc0fa8e99
naperdish <pihuihush@1hokage.ru>
2023-11-01 13:53:50 +0300

    second

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,4 +1,25 @@
 package ru.netology.rest;
 
-public class PostmanEchoTest {
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hi"))
+        ;
+    }
 }
